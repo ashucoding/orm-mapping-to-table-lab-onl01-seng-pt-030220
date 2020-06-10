f5d5a7c07b76f9b47a0dd3492c0594633df12c9a
Shana Wiltshire <shanawiltshire@gmail.com>
2020-06-10 00:07:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,13 +8,13 @@ class Student
       @grade = grade
     end
 
-    def .create_table
+    def self.create_table
       sql =  <<-SQL
      CREATE TABLE IF NOT EXISTS students (
        id INTEGER PRIMARY KEY,
        name TEXT,
        grade TEXT
-     );
+     )
        SQL
    DB[:conn].execute(sql)
     end
